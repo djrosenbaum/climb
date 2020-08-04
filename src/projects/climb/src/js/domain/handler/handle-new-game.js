21379e34202e5cfed5d5e263f0fc7037208aedad
DJ Rosenbaum <djrosenbaum@gmail.com>
2020-08-04 08:53:07 -0400

    handle new game

--- src/projects/climb/src/js/domain/handler/handle-new-game.js
+++ src/projects/climb/src/js/domain/handler/handle-new-game.js
@@ -7,8 +7,11 @@ export default async function handleNewGame() {
 
   console.log('handle new game');
   // try setting room code
-  const rooms = firebase.database().ref('rooms');
-  rooms.set(createNewRoom());
+  app.roomCode = getRoomCode();
+  const rooms = firebase.database().ref(`rooms/${app.roomCode}`);
+  rooms.set({
+    foo: 'bar'
+  });
 }
 
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -26,13 +29,13 @@ function getRandom(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function createNewRoom() {
-  const room = {};
-  const roomCode = getRoomCode();
-  room[roomCode] = {
-    foo: 'bar',
-  }
-  app.roomCode = roomCode;
-  console.log('created room', room);
-  return room;
-}
+// function createRoom() {
+//   const room = {};
+//   const roomCode = getRoomCode();
+//   room[roomCode] = {
+//     foo: 'bar',
+//   }
+//   app.roomCode = roomCode;
+//   console.log('created room', room);
+//   return room;
+// }
