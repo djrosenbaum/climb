43af71901d23a79e11c8751d6605ada8d9a68dfe
DJ Rosenbaum <djrosenbaum@gmail.com>
2020-08-16 09:25:43 -0400

    set player name

--- src/projects/climb/src/js/domain/handler/handle-join-game.js
+++ src/projects/climb/src/js/domain/handler/handle-join-game.js
@@ -3,38 +3,58 @@ import displayScreen from '../markup/display-screen';
 
 export default async function handleJoinGame() {
   console.log('handling join game');
-  if (app.myPlayerIndex) {
+  if (app.playerKey) {
     return;
   }
 
   const roomCode = getRoomCode();
+  const playerName = getPlayerName();
   console.log('room code:', roomCode);
+  console.log('player name:', playerName);
 
-  const room = firebase.database().ref(`rooms/${roomCode}`);
+  if (!roomCode || !playerName) {
+    // room code or player are required
+    console.log('room code and player name are required');
+    return;
+  }
 
-  // get the room code
-  room.once('value').then(function(snapshot) {
-    console.log(snapshot.val());
-  });
+  // firebaseRoomReference = firebase.database().ref(`rooms/${roomCode}`);
+  app.room = firebase.database().ref(`rooms/${roomCode}`);
 
-  // set the room code
-
-  // get a snapshot of the room
-
-  // verify the room is accepting players
-
-  // set your player info
-
-  // app.myPlayerIndex = getMyPlayerIndex();
+  // get the room data from firebase
+  app.room.once('value').then(setPlayerName);
 }
 
-function getMyPlayerIndex() {
-  // gets the player index
-  console.log('get my player index');
+/**
+ * 
+ * @param {Object} snapshot - some firebase snapshot
+ */
+function setPlayerName(snapshot) {
+  console.log('setting player name');
+  console.log(snapshot.val());
+  const { status } = snapshot.val() || {};
+  if (status !== 'waiting') {
+    // can only join a room waiting for players
+    console.log('room is closed to new players');
+    return;
+  }
+  // set your player info
+  app.room.child('players').push({
+    playerName: getPlayerName(),
+  }).then((data) => {
+    console.log('player set', app.playerKey);
+    app.playerKey = data.getKey();
+  });
 }
 
 function getRoomCode() {
-  let roomCode = document.querySelector('[data-input="room-code"]').value;
-  
-  return roomCode.toLowerCase();
+  console.log('get room code');
+  let roomCode = document.querySelector('[data-input="room-code"]').value;  
+  return roomCode.toLowerCase(); 
+}
+
+function getPlayerName() {
+  console.log('get player name');
+  let playerName = document.querySelector('[data-input="player-name"]').value || 'guest';
+  return playerName.toLowerCase();
 }
